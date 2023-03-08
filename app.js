ea182ae9cf524ce0c5b5de15a94ecde02cd17796
Dewesh Jha <deweshjha4@gmail.com>
2023-03-08 18:32:59 +0530

    update package.json

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const swaggerDoc = require("./json/swagger.json");
 //app settings
 const app = express();
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("views", "views"); 
 
 
 app.use(express.urlencoded({ extended: true }));
